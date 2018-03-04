bfca0694a047414f938d145d95253fca3aa7ae50
Ujitoko <yusuke.ujitoko@gmail.com>
2018-03-04 22:31:56 +0900

    add

---
+++ wavenet/models.py
@@ -0,0 +1,283 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from wavenet.layers import (_causal_linear, _output_linear, conv1d,
+                    dilated_conv1d)
+from wavenet.utils import *
+import os
+import numpy as np
+
+class Model(object):
+    def __init__(self,
+                 num_time_samples,
+                 num_channels=1,
+                 num_classes=256,
+                 num_blocks=2,
+                 num_layers=14,
+                 num_hidden=128,
+                 gpu_num = "0",
+                 model_name = "default_name"):
+        
+        self.num_time_samples = num_time_samples
+        self.num_channels = num_channels
+        self.num_classes = num_classes
+        self.num_blocks = num_blocks
+        self.num_layers = num_layers
+        self.num_hidden = num_hidden
+        self.gpu_num = gpu_num
+        self.model_name = model_name
+        
+        inputs = tf.placeholder(tf.float32,
+                                shape=(None, num_time_samples, num_channels))
+        targets = tf.placeholder(tf.int32, shape=(None, num_time_samples))
+
+        h = inputs
+        hs = []
+        for b in range(num_blocks):
+            for i in range(num_layers):
+                rate = 2**i
+                name = 'b{}-l{}'.format(b, i)
+                h = dilated_conv1d(h, num_hidden, rate=rate, name=name)
+                hs.append(h)
+
+        outputs = conv1d(h,
+                         num_classes,
+                         filter_width=1,
+                         gain=1.0,
+                         activation=None,
+                         bias=True)
+
+        costs = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            logits=outputs, labels=targets)
+        cost = tf.reduce_mean(costs)
+
+        train_step = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
+
+        self.config = tf.ConfigProto(
+            gpu_options=tf.GPUOptions(
+                visible_device_list="0"
+            )
+        )
+        sess = tf.Session(config=self.config)
+        sess.run(tf.global_variables_initializer())
+        self.inputs_ph = inputs
+        self.targets_ph = targets
+        self.outputs = outputs
+        self.hs = hs
+        self.costs = costs
+        self.cost = cost
+        self.train_step = train_step
+        self.sess = sess
+
+        
+        # dataset
+        self.ad_train = AccelerationDataset("acc_dataset_selected/train")
+        self.batch_size = 2
+        
+        self.generate_init()
+        self.count = 0
+        
+
+    def _train(self):
+        batch_x, batch_y = self.ad_train.getBatchTrain(True, self.num_time_samples, self.batch_size)
+        batch_x = batch_x.reshape((self.batch_size, self.num_time_samples, 10))#self.num_input))        
+        batch_x = batch_x[:,:,0]#.reshape((self.batch_size, self.num_time_samples,-1))
+        
+        bins = np.linspace(-1, 1, 256)
+        # Quantize inputs.
+                
+        inputs_batch, targets_batch = [],[]
+        for batch in range(self.batch_size):
+            x = batch_x[batch]
+            y = batch_y[batch]
+            inputs = np.digitize(x, bins, right=False) - 1
+            inputs = bins[inputs][None, :, None]
+            inputs_batch.append(inputs)
+        
+            # Encode targets as ints.
+            targets = (np.digitize(y, bins, right=False) - 1)[None, :]
+            targets_batch.append(targets)
+
+        inputs_batch = np.vstack(inputs_batch)
+        targets_batch = np.vstack(targets_batch)
+
+        # save for test
+        if self.count == 0:
+            self.test_inputs = inputs_batch
+            self.test_targets = targets_batch
+            self.count += 1
+
+        feed_dict = {self.inputs_ph: inputs_batch, self.targets_ph: targets_batch}
+        cost, _ = self.sess.run(
+            [self.cost, self.train_step],
+            feed_dict=feed_dict)
+        return cost
+
+    def train(self):
+        losses = []
+        terminal = False
+        i = 0
+        while not terminal:
+            i += 1                        
+            cost = self._train()
+            
+            if cost < 1e-1:
+                terminal = True
+            losses.append(cost)
+            
+            if i % 50 == 0:
+                self.generate_run(self.test_inputs[0,:,:][np.newaxis,:,:], i)
+                print(cost)
+
+                show_wave(losses, dirname=self.model_name, filename="losses_" + str(i), y_lim=7)
+                #plt.plot(losses)
+                #plt.ylim(0, 7)
+                #plt.show()
+                
+                
+    def generate_init(self, batch_size=1):
+        self.bins = np.linspace(-1, 1, self.num_classes)
+        input_size = 1 #self.num_time_samples
+        inputs = tf.placeholder(tf.float32, [batch_size, input_size],
+                                name='inputs')
+
+        print('Make Generator.')
+
+        count = 0
+        h = inputs
+
+        init_ops = []
+        push_ops = []
+        for b in range(self.num_blocks):
+            for i in range(self.num_layers):
+                rate = 2**i
+                name = 'b{}-l{}'.format(b, i)
+                if count == 0:
+                    state_size = 1
+                else:
+                    state_size = self.num_hidden
+                    
+                q = tf.FIFOQueue(rate,
+                                 dtypes=tf.float32,
+                                 shapes=(batch_size, state_size))
+                init = q.enqueue_many(tf.zeros((rate, batch_size, state_size)))
+
+                state_ = q.dequeue()
+                push = q.enqueue([h])
+                init_ops.append(init)
+                push_ops.append(push)
+
+                h = _causal_linear(h, state_, name=name, activation=tf.nn.relu)
+                count += 1
+
+        outputs = _output_linear(h)
+
+        out_ops = [tf.nn.softmax(outputs)]
+        out_ops.extend(push_ops)
+
+        self.inputs = inputs
+        self.init_ops = init_ops
+        self.out_ops = out_ops
+        
+        # Initialize queues.
+        self.sess.run(self.init_ops)
+
+    def generate_run(self, inputs, i):
+        predictions = []
+        
+        num_samples = inputs.shape[1] # time_steps
+        for step in range(num_samples):
+            input = inputs[:,step,:].reshape([1,1])
+            feed_dict = {self.inputs: input}
+            output = self.sess.run(self.out_ops, feed_dict=feed_dict)[0] # ignore push ops
+            value = np.argmax(output[0, :])
+
+            #input = np.array(self.bins[value])[None, None] # TODO
+            value_binned = np.array(self.bins[value])[None, None]
+            predictions.append(value_binned)
+
+        #if step % 10000 == 0:
+        predictions_ = np.concatenate(predictions, axis=1)
+        show_wave(predictions_[0, :], dirname=self.model_name, filename="gen_"+str(i))
+        #plt.plot(predictions_[0, :], label='pred')
+        #plt.legend()
+        #plt.xlabel('samples from start')
+        #plt.ylabel('signal')
+        #plt.show()
+
+        predictions_ = np.concatenate(predictions, axis=1)
+        return predictions_        
+        
+        
+"""
+class Generator(object):
+    def __init__(self, model, batch_size=1, input_size=1):
+        self.model = model
+        self.bins = np.linspace(-1, 1, self.model.num_classes)
+
+        inputs = tf.placeholder(tf.float32, [batch_size, input_size],
+                                name='inputs')
+
+        print('Make Generator.')
+
+        count = 0
+        h = inputs
+
+        init_ops = []
+        push_ops = []
+        for b in range(self.model.num_blocks):
+            for i in range(self.model.num_layers):
+                rate = 2**i
+                name = 'b{}-l{}'.format(b, i)
+                if count == 0:
+                    state_size = 1
+                else:
+                    state_size = self.model.num_hidden
+                    
+                q = tf.FIFOQueue(rate,
+                                 dtypes=tf.float32,
+                                 shapes=(batch_size, state_size))
+                init = q.enqueue_many(tf.zeros((rate, batch_size, state_size)))
+
+                state_ = q.dequeue()
+                push = q.enqueue([h])
+                init_ops.append(init)
+                push_ops.append(push)
+
+                h = _causal_linear(h, state_, name=name, activation=tf.nn.relu)
+                count += 1
+
+        outputs = _output_linear(h)
+
+        out_ops = [tf.nn.softmax(outputs)]
+        out_ops.extend(push_ops)
+
+        self.inputs = inputs
+        self.init_ops = init_ops
+        self.out_ops = out_ops
+        
+        # Initialize queues.
+        self.model.sess.run(self.init_ops)
+
+    def run(self, input, num_samples):
+        predictions = []
+        for step in range(num_samples):
+
+            feed_dict = {self.inputs: input}
+            output = self.model.sess.run(self.out_ops, feed_dict=feed_dict)[0] # ignore push ops
+            value = np.argmax(output[0, :])
+
+            input = np.array(self.bins[value])[None, None] # TODO
+            predictions.append(input)
+
+            if step % 1000 == 0:
+                predictions_ = np.concatenate(predictions, axis=1)
+                plt.plot(predictions_[0, :], label='pred')
+                plt.legend()
+                plt.xlabel('samples from start')
+                plt.ylabel('signal')
+                plt.show()
+
+        predictions_ = np.concatenate(predictions, axis=1)
+        return predictions_
+"""
---
+++ wavenet/utils.py
@@ -0,0 +1,158 @@
+import numpy as np
+
+from scipy.io import wavfile
+import os
+import matplotlib.pyplot as plt
+
+def normalize(data):
+    temp = np.float32(data) - np.min(data)
+    out = (temp / np.max(temp) - 0.5) * 2
+    return out
+
+
+def make_batch(path):
+    data = wavfile.read(path)[1][:, 0]
+
+    data_ = normalize(data)
+    # data_f = np.sign(data_) * (np.log(1 + 255*np.abs(data_)) / np.log(1 + 255))
+
+    bins = np.linspace(-1, 1, 256)
+    print(data_[0:-1].shape)
+    print(data_[1::].shape)
+    # Quantize inputs.
+    inputs = np.digitize(data_[0:-1], bins, right=False) - 1
+    inputs = bins[inputs][None, :, None]
+
+    # Encode targets as ints.
+    targets = (np.digitize(data_[1::], bins, right=False) - 1)[None, :]
+    return inputs, targets
+
+def one_hot_encoding(t_vec, num_classes):
+    #t_oh = np.zeros((len(t_vec), num_classes)).astype(int)
+    #t_oh[np.arange(len(t_vec)), t_vec] = 1
+    #return t_oh
+    targets = np.array(t_vec).reshape(-1)
+    t_oh = np.eye(num_classes)[targets].astype("int")
+    return (t_oh)
+
+Selected_Acceleration_Label = {
+    "G1SquaredAluminumMesh":0,
+    "G2GraniteTypeVeneziano":1,
+    "G3AluminumPlate":2,
+    "G4Bamboo":3,
+    "G5SolidRubberPlateVersion1":4,
+    "G6Carpet":5,
+    "G7FineFoamVersion2":6,
+    "G8Cardboard":7,
+    "G9Jeans":8,
+    }    
+
+class AccelerationDataset():
+    
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.files = os.listdir(self.root_dir)
+        self.files_z = [file for file in self.files if (file.find("_Z_") > 0) and (file.find("acc") > 0)]
+        
+    def len(self):
+        #return len(self.files_z)
+        return 1
+
+    def getitem(self, idx):
+        # wave
+        file_name_acc_z = self.files_z[idx][:-7]
+        pos_Z_ = file_name_acc_z.find("_Z_")
+
+        file_name_vel_x = file_name_acc_z[:pos_Z_] + "_X_" + file_name_acc_z[pos_Z_ + len("_Z_"):]
+        file_name_vel_y = file_name_acc_z[:pos_Z_] + "_Y_" + file_name_acc_z[pos_Z_ + len("_Z_"):]
+        file_name_vel_mag = file_name_acc_z[:pos_Z_] + "_mag_" + file_name_acc_z[pos_Z_ + len("_Z_"):]
+
+        z = np.load(os.path.join(self.root_dir, file_name_acc_z + "acc.npy")) * 2.0 - 1.0
+        x = np.load(os.path.join(self.root_dir, file_name_vel_x + "vel.npy"))
+        y = np.load(os.path.join(self.root_dir, file_name_vel_y + "vel.npy"))
+        mag_vel = np.load(os.path.join(self.root_dir, file_name_vel_mag + "vel.npy"))
+
+        #stacked = np.stack((x,y))
+        #norm_vel = np.linalg.norm(np.stack((x,y)), axis=0)
+        #print(norm_vel.shape)
+        wave = np.stack((x,y,z,mag_vel))
+
+        # label
+        pos_label = file_name_acc_z.find("_Movement_")
+        label_name = file_name_acc_z[:pos_label]
+        label_num = Selected_Acceleration_Label[label_name]
+        label_one_hot = one_hot_encoding(label_num, 9)
+        
+        sample = {"wave": wave, "label_num": label_num, "label_name": label_name, "label_one_hot": label_one_hot}
+        return sample
+
+    
+    def getBatchTrain(self, is_random, time_step, batch_size):
+        rand_batch_idx = np.random.randint(0, self.len(), size=batch_size)
+        #print(rand_batch_idx)
+
+        batch_input = []
+        batch_output = []
+        for idx in rand_batch_idx:
+            item = self.getitem(idx)
+            rand_time_start = 0#np.random.randint(0, item["wave"].shape[1] - time_step)
+            #print(item["wave"].shape)
+            wave_time_series = item["wave"][3,rand_time_start:rand_time_start+time_step][np.newaxis,:].transpose()
+            one_hot = item["label_one_hot"]
+            one_hot_time_series = np.repeat(one_hot, time_step, axis=0)
+
+            input = np.concatenate((wave_time_series, one_hot_time_series), axis=1)
+            #print(input.shape)
+            batch_input.append(input)
+            batch_output.append(item["wave"][2,rand_time_start+1: rand_time_start+time_step+1].transpose())
+            
+        batch_input_np = np.array(batch_input)
+        batch_output_np = np.array(batch_output)
+        return batch_input_np, batch_output_np#[:, np.newaxis]
+
+    
+    def getSequentialItem(self, time_step, batch_size=2):
+        #rand_batch_idx = np.random.randint(0, self.len()-1, size=batch_size)
+        #print(rand_batch_idx)
+        batch_idx = np.arange(batch_size)
+        
+        batch_input = []
+        batch_output = []
+
+        item = self.getitem(0)
+        time_series = item["wave"].shape[1]-batch_size
+        #print(time_step)
+        #print(batch_size)
+        #print(time_series)
+        for time in np.arange(batch_size):
+            time_start = 0
+            
+            wave_time_series = item["wave"][3,time_start + time:time_start+time+time_step][np.newaxis,:].transpose()
+            one_hot = item["label_one_hot"]
+            one_hot_time_series = np.repeat(one_hot, time_step, axis=0)
+
+            input = np.concatenate((wave_time_series, one_hot_time_series), axis=1)
+            #print(input.shape)
+            batch_input.append(input)
+            batch_output.append(item["wave"][2,time_start+time+time_step].transpose())
+            
+        batch_input_np = np.array(batch_input)
+        batch_output_np = np.array(batch_output)
+        return batch_input_np, batch_output_np[:, np.newaxis]
+    
+def show_wave(wave, dirname, filename, y_lim=0):
+    if os.path.isdir(dirname) == False:
+        os.mkdir(dirname)
+    
+    plt.figure(figsize=(30,10))
+    plt.title('wave files', fontsize=30)
+    plt.xlabel('x', fontsize=20)
+    plt.ylabel('y', fontsize=20)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    if y_lim != 0:
+        plt.ylim(0, y_lim)
+    plt.plot(wave, color='r',linewidth=1.0)
+    plt.savefig(os.path.join(dirname,filename + '.png'))
+    plt.close()
+
